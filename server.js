b8a73a0c1a7b8517b386be56b9c23f24541a8298
Kataria212005 <2023.hanishka.kataria@ves.ac.in>
2026-08-27 13:43:33 +0530

    changed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 function readEnv(name) {
   const raw = process.env[name];
@@ -37,6 +37,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 /* =========================
    MONGODB CONNECTION
